29b394741e1b41b3d903b913e7397373d4f667ba
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-02-08 17:03:56 +0900

    Test
    테스트입니다.

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C언어 콘서트
+//
+//  Created by 이승수 on 2/8/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
