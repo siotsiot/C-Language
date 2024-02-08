8aeb50bd8b5a841a7c6703014923f0f924d3bf4f
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-02-08 17:45:57 +0900

    Update 4.c
    사용자로부터 정수를 입력받아서 계속 더하는 프로그램을 작성해보자. 사용자가 0을 입력하면 지금까지 입력된 모든 정수의 합계를 출력하고 종료한다.

--- Chapter 6 반복문/Programming/4.c
+++ Chapter 6 반복문/Programming/4.c
@@ -2,6 +2,18 @@
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    int num, sum = 0;
+    
+    while (1)
+    {
+        printf("정수를 입력하시오: ");
+        scanf("%d", &num);
+        if (num == 0)
+            break;
+        sum += num;
+        
+    }
+    printf("합계=%d\n", sum);
+    
     return 0;
 }
