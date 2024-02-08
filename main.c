1a9a9d8eb22fe6ca6afddcea8e04ddbdb5a80c21
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-02-08 17:05:43 +0900

    test1
    수정

--- main.c
+++ main.c
@@ -1,14 +1,7 @@
-//
-//  main.c
-//  C언어 콘서트
-//
-//  Created by 이승수 on 2/8/24.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main(void)
+{
     printf("Hello, World!\n");
     return 0;
 }
