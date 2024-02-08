4063207293ee7e8a7a77ccbb301032ed9e5464b8
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-02-08 21:45:35 +0900

    Update No. 11.c
    중첩 반복문을 사용하여서 다음과 같이 출력하는 프로그램을 작성하여 보자.

--- Chapter 6 반복문/Programming/No. 11.c
+++ Chapter 6 반복문/Programming/No. 11.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int num, i, j;
+    
+    printf("정수를 입력하시오: ");
+    scanf("%d", &num);
+    
+    for (i = 1; i <= num; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%3d", j);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
