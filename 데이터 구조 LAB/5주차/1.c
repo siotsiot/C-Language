5008f20c943d2556367d52a1f62279758253a886
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-04-04 09:35:55 +0900

    Create 1.c

---
+++ 데이터 구조 LAB/5주차/1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 20;
+    int* p1, * p2;
+
+    p1 = &a;
+    printf("p1 = &a;\n");
+    printf(" a의 주소: %p\t a의 값: %d\n", &a, a);
+    printf(" b의 주소: %p\t b의 값: %d\n", &b, b);
+    printf("p1의 주소: %p\tp1의 값: %d\n", p1, *p1);
+    //printf("p2의 주소: %p\tp2의 값: %d\n", p2, *p2);
+    printf("\n");
+
+    p2 = &b;
+    printf("p2 = &b;\n");
+    printf(" a의 주소: %p\t a의 값: %d\n", &a, a);
+    printf(" b의 주소: %p\t b의 값: %d\n", &b, b);
+    printf("p1의 주소: %p\tp1의 값: %d\n", p1, *p1);
+    printf("p2의 주소: %p\tp2의 값: %d\n", p2, *p2);
+    printf("\n");
+
+    b = *p1;
+    printf("b = *p1;\n");
+    printf(" a의 주소: %p\t a의 값: %d\n", &a, a);
+    printf(" b의 주소: %p\t b의 값: %d\n", &b, b);
+    printf("p1의 주소: %p\tp1의 값: %d\n", p1, *p1);
+    printf("p2의 주소: %p\tp2의 값: %d\n", p2, *p2);
+    printf("\n");
+
+    *p2 = 20;
+    printf("*p2 = 20;\n");
+    printf(" a의 주소: %p\ta의 값: %d\n", &a, a);
+    printf(" b의 주소: %p\tb의 값: %d\n", &b, b);
+    printf("p1의 주소: %p\tp1의 값: %d\n", p1, *p1);
+    printf("p2의 주소: %p\tp2의 값: %d\n", p2, *p2);
+    printf("\n");
+
+    *p1 = *p2;
+    printf("*p1 = *p2;\n");
+    printf(" a의 주소: %p\t a의 값: %d\n", &a, a);
+    printf(" b의 주소: %p\t b의 값: %d\n", &b, b);
+    printf("p1의 주소: %p\tp1의 값: %d\n", p1, *p1);
+    printf("p2의 주소: %p\tp2의 값: %d\n", p2, *p2);
+    printf("\n");
+
+    p2 = p1;
+    printf("p2 = &b;\n");
+    printf(" a의 주소: %p\t a의 값: %d\n", &a, a);
+    printf(" b의 주소: %p\t b의 값: %d\n", &b, b);
+    printf("p1의 주소: %p\tp1의 값: %d\n", p1, *p1);
+    printf("p2의 주소: %p\tp2의 값: %d\n", p2, *p2);
+    printf("\n");
+
+    *p2 = 10;
+    printf("*p2 = 10;\n");
+    printf(" a의 주소: %p\t a의 값: %d\n", &a, a);
+    printf(" b의 주소: %p\t b의 값: %d\n", &b, b);
+    printf("p1의 주소: %p\tp1의 값: %d\n", p1, *p1);
+    printf("p2의 주소: %p\tp2의 값: %d\n", p2, *p2);
+
+    return 0;
+}
