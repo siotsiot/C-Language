e476523fe8840c0d5f8025bb8757efe9e3582bf2
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-05-12 16:41:10 +0900

    Create 2-e.c

---
+++ 데이터 구조 LAB/11주차/2-e.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodeType {
+    int val;
+    struct nodeType* next;
+} nodeType;
+
+int main(void)
+{
+    nodeType* first;
+    first = (nodeType*)malloc(sizeof(nodeType));
+    first->val = 10;
+
+    first->next = (nodeType*)malloc(sizeof(nodeType));
+    first->next->val = 20;
+
+    first->next->next = (nodeType*)malloc(sizeof(nodeType));
+    first->next->next->val = 30;
+    first->next->next->next = NULL;
+
+    nodeType* current = first; // current가 처음 노드를 가리킴.
+    while (current != NULL)
+    {
+        printf("%d ", current->val);
+        current = current->next;
+    }
+
+    current = first; // 다시 처음 노드를 가리킴.
+    while (current != NULL)
+    {
+        nodeType* temp = current->next;
+        free(current);
+        current = temp;
+    }
+
+    return 0;
+}
