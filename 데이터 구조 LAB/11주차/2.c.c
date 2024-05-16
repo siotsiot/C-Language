89134946cc5ec6029bcb0cc3ff176c4e8f319845
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-05-16 17:22:41 +0900

    Create 2.c.c

---
+++ 데이터 구조 LAB/11주차/2.c.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodetype {
+    int val;
+    struct nodetype* link;
+} nodetype;
+
+nodetype* insert_first(nodetype* list, int val)
+{
+    nodetype* p = (nodetype*)malloc(sizeof(nodetype));
+    p->val = val;
+    p->link = list;
+    return p;
+}
+
+nodetype* insert_last(nodetype* list, int val)
+{
+    nodetype* p = (nodetype*)malloc(sizeof(nodetype));
+    p->val = val;
+    p->link = NULL;
+
+    if (list == NULL)
+        return p;
+
+    nodetype* temp = list;
+    while (temp->link != NULL)
+    {
+        temp = temp->link;
+    }
+    temp->link = p;
+    return list;
+}
+
+nodetype* cons_list(nodetype* list)
+{
+    list = insert_last(list, 5);
+    list = insert_last(list, 6);
+    list = insert_last(list, 7);
+    
+    return list;
+}
+
+void print_list(nodetype* list)
+{
+    nodetype* p = list;
+    
+    while (p != NULL)
+    {
+        printf("%d", p->val);
+        if (p->link != NULL)
+            printf(">");
+        p = p->link;
+    }
+    printf("\n");
+}
+nodetype* delete_first(nodetype* list)
+{
+    nodetype* p;
+
+    if (list == NULL)
+    {
+        fprintf(stderr, "리스트가 비었습니다.\n");
+        exit(1);
+    }
+    else
+    {
+        p = list;
+        list = p->link;
+        free(p);
+    }
+    return list;
+}
+
+nodetype* delete_last(nodetype* list)
+{
+    if (list == NULL)
+    {
+        fprintf(stderr, "리스트가 비었습니다.\n");
+        exit(1);
+    }
+    else
+    {
+        nodetype* temp = list;
+        while (temp->link->link != NULL)
+            temp = temp->link;
+        free(temp->link);
+        temp->link = NULL;
+        return list;
+    }
+}
+
+int main(void)
+{
+    nodetype* head = NULL;
+    head = cons_list(head);
+    printf("After list construction using last insertion\n");
+    print_list(head);
+    printf("\nAfter deletion using last deletion\n");
+    head = delete_last(head);
+    print_list(head);
+
+    return 0;
+}
